4d672782e0b62b6ad60645d8dc45918b0386a216
Hobrus <bhobrus@gmail.com>
2024-03-11 19:32:07 +0300

    upd 1

--- Portal/Disks/models.py
+++ Portal/Disks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class GenreChoice(models.TextChoices):
+    POP = 'POP', 'Pop'
+    ROCK = 'RK', 'Rock'
+    JAZZ = 'JZ', 'Jazz'
+    CLASSIC = 'CL', 'Classic'
+
+
+class Disk(models.Model):
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=3, choices=GenreChoice.choices, default=GenreChoice.CLASSIC)
+    title = models.CharField(max_length=100)
